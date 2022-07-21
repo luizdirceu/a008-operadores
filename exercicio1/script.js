cb2163c8d78f5ab4823bdb1c0bbcfcbb5d9f766e
Luiz Dirceu <juninholdmarques@gmail.com>
2022-07-21 18:12:56 -0300

    minha atividade 008

---
+++ exercicio1/script.js
@@ -0,0 +1,8 @@
+let numeropar = +prompt ("Digite um numero par")
+let divisaonumeropar = numeropar / 2
+let resto = divisaonumeropar % 2
+console.log (resto)
+
+// OUVE UM PADRAO NO RESTO DA DIVISAO, VARIANDO ENTRE 1 E 0
+
+// SE DIGITAR UM NUMERO IMPAR ELE SAI DO PADRAO 1 OU 0 E VAI PARA UM PADRAO 1.5 OU 0.5
---
+++ exercicio3/script.js
@@ -0,0 +1,14 @@
+const operacao = 5 > 20 && 5 > 2
+console.log (operacao && operacao)
+
+const operacao2 = 5 === 5 || 5 === "5"
+console.log (operacao2) 
+// RETORNOU TRUE POIS SE TRATA DE UM OPERADOR LOGICO "OU" QUE RETORNA FALSE SE TODAS AS OPERAÇÕES FOREM FALSA.
+
+const operacao3 = !20 > 50
+console.log (operacao3)
+
+const operacao4 = !(20 > 50)
+console.log(operacao4)
+
+// TRUE POR QUE O NEGAÇÃO SEMPRE RETORNA O OPOSTO 
